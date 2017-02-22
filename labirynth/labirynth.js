6b3546a8fe46b0962f92c3273e8f5faf9597a98d
Marcin Pasternak <ucatunit@gmail.com>
2017-02-22 22:40:34 +0100

    new functions coming soon

--- labirynth/labirynth.js
+++ labirynth/labirynth.js
@@ -13,7 +13,6 @@ var generateHashBox = function (size) {
 };
 
 var printLabirynth = function (labFile) {
-
 	for ( var i = 0; i < labFile.length; i ++) {
 		console.log( i ,labFile[i].join(''));
 	}
@@ -23,3 +22,24 @@ x = generateHashBox(10);
 
 
 printLabirynth(x);
+
+
+var generateLabirynth = function (hashBox) {
+	start = pickStart(hashBox); // specify starting point and start from there
+
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function pickStart (arr) {
+	if ( getRandomInt(0, 1)%2 == 0) {
+		// start from row
+		
+	}
+	selected_row = getRandomInt(1, arr.length - 1);
+	console.log(selected_row);
+}
+
+pickStart(x);
